14a4f2d4028861ff641979b334dfb337799242a8
blepoxp <blepoxp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-06 21:17:42 +0000

    Moved Signature management to Users Tab
    git-svn-id: https://plugins.svn.wordpress.org/ft-signature-manager/tags/1.1@63010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ft_signature_manager.php
+++ ft_signature_manager.php
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-define( 'FT_Signature_Manager_Vesion' , '1.0' );
+define( 'FT_Signature_Manager_Vesion' , '1.1' );
 
 // Setup form security
 if ( !function_exists('wp_nonce_field') ) {
